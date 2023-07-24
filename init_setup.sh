82d3a04d7baa400c2e4d3e309668189783f6c073
RAVIRAJ KUKADE <ravirajkukade11@gmail.com>
2023-07-24 14:25:33 +0530

    added some more requiremntes

--- init_setup.sh
+++ init_setup.sh
@@ -1,8 +1,8 @@
 echo [$(date)]: 'START'
 echo [$(date)]: 'CREATING ENV WITH PYTHON 3.8'
-conda create --prefix ./env python=3.8 -y
+conda create --prefix deep_pro python=3.8 -y
 echo [$(date)]: 'ACTIVATING THE ENV'
-source activate ./env
+source activate deep_pro
 echo [$(date)]: 'INSTALLING THE DEV REQUIREMENTS'
 pip install -r requirements_dev.txt
 echo [$(date)]: 'END'
