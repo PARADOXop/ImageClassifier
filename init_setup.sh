185cca31e3afd42b7bf2011f756725d5a04c65ca
RAVIRAJ KUKADE <ravirajkukade11@gmail.com>
2023-07-31 15:33:32 +0530

    started again

--- init_setup.sh
+++ init_setup.sh
@@ -1,8 +1,8 @@
-echo [$(date)]: 'START'
-echo [$(date)]: 'CREATING ENV WITH PYTHON 3.8'
-conda create --prefix deep_pro python=3.8 -y
-echo [$(date)]: 'ACTIVATING THE ENV'
-source activate deep_pro
-echo [$(date)]: 'INSTALLING THE DEV REQUIREMENTS'
+echo [$(date)]: "START" 
+echo [$(date)]: "creating env with python 3.8 version" 
+conda create --prefix ./env python=3.8 -y
+echo [$(date)]: "activating the environment" 
+source activate ./env
+echo [$(date)]: "installing the dev requirements" 
 pip install -r requirements_dev.txt
-echo [$(date)]: 'END'
+echo [$(date)]: "END" 
--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -4,23 +4,23 @@ from pathlib import Path
 from box import ConfigBox
 from ensure.main import EnsureError
 
-yaml_files = ['tests\Data\empty.yaml',
-    'tests\Data\demo.yaml'
-              ]
 
 class Test_read_yaml:
+    yaml_files = [
+        "tests/data/empty.yaml",
+        "tests/data/demo.yaml"
+    ]
+
     def test_read_yaml_empty(self):
         with pytest.raises(ValueError):
-            read_yaml(Path(yaml_files[0]))
+            read_yaml(Path(self.yaml_files[0]))
 
-            
     def test_read_yaml_return_type(self):
-        response = read_yaml(Path(yaml_files[-1]))
-        assert isinstance(response, ConfigBox)
+        respone = read_yaml(Path(self.yaml_files[-1]))
+        assert isinstance(respone, ConfigBox)
 
-
-    @pytest.mark.parametrize('path_to_yaml', yaml_files)
-    def test_read_yaml_bad_dataType(self,path_to_yaml):
+    @pytest.mark.parametrize("path_to_yaml", yaml_files)
+    def test_read_yaml_bad_type(self, path_to_yaml):
         with pytest.raises(EnsureError):
-            read_yaml((path_to_yaml))
+            read_yaml(path_to_yaml)
 
