2a0db7eedbaa73286cbad4067214e56434b8cc14
RAVIRAJ KUKADE <ravirajkukade11@gmail.com>
2023-07-31 15:44:21 +0530

    ..

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -7,8 +7,8 @@ from ensure.main import EnsureError
 
 class Test_read_yaml:
     yaml_files = [
-        "tests/data/empty.yaml",
-        "tests/data/demo.yaml"
+        "tests/Data/empty.yaml",
+        "tests/Data/demo.yaml"
     ]
 
     def test_read_yaml_empty(self):
