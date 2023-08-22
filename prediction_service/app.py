080e09afa9d9f054bd25747604edf33d0757d2ab
RAVIRAJ KUKADE <46750611+PARADOXop@users.noreply.github.com>
2023-08-22 16:34:22 +0530

    Update app.py

--- prediction_service/app.py
+++ prediction_service/app.py
@@ -8,7 +8,7 @@ import numpy as np
 # deep Classifier project
 
 """
-model = tf.keras.models.load_model("model.h5")
+model = tf.keras.models.load_model("./model.h5")
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     # To read file as bytes:
